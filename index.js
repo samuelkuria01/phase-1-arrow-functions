7a3bd53cc10b598fe51df3d7df7d661a14834d63
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2022-11-30 08:54:56 +0300

    we aiming high

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide=(num1,num2) =>{
+    return (num1/num2);
+} 
+divide(2000,100);
+
+
+let x=2;
+const square=(num1)=>{
+    return(num1*x);
+}
+multiplies(2);
+
+
+const add=(a, b)=>(a+b);
+add(3,4);
+
+
+
+
